ce08fd09f7cb094de34eb77806e5d3239803ca0d
jwaynick99 <jwaynick99@gmail.com>
2024-09-09 14:34:48 -0500

    Chapter 3 Control flow and Collections Studio

---
+++ control-flow-and-collections/studio/counting-characters/src/main/java/org/launchcode/CountChars.java
@@ -0,0 +1,30 @@
+package org.launchcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountChars {
+    public static void main(String[] args){
+        String loopString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with." +
+                " So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of" +
+                " the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+
+        char[] charInString = loopString.toLowerCase().toCharArray();
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        char alphabetArr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        for (char character : charInString) {
+            int count = 0;
+
+            for (char singleChar : charInString){
+                if (singleChar == character){
+                    count++;
+                }
+            }
+            charCount.put(character, count);
+        }
+        for (Map.Entry<Character, Integer> charPrint : charCount.entrySet()){
+            System.out.println(charPrint.getKey() + " : " + charPrint.getValue());
+        }
+    }
+}
