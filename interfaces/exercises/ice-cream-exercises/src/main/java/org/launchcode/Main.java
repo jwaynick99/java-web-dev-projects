361b9627a92b11e0cbf3913fca1e0dfc49cdde8d
jwaynick99 <jwaynick99@gmail.com>
2024-09-26 00:01:58 -0500

    Chapter 8 Exercises - Interfaces

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -7,11 +7,17 @@ public class Main {
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
-
+        FlavorComparator comparator = new FlavorComparator();
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
-
+        flavors.sort(comparator);
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
-
+        cones.sort(new ConeComparator());
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        for (Flavor flavor : flavors){
+            System.out.println(flavor.getName() + "; Allergens: " + flavor.getAllergens());
+        }
+        for (Cone cone : cones){
+            System.out.println(cone.getName() + ": $" + cone.getCost());
+        }
     }
 }
